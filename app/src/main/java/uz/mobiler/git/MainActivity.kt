dee8f8785576ca55c2e8cb5cf1966a0365a71d53
sanjar_suvonov <sanjar.tatu.96@gmail.com>
2021-09-25 11:28:28 +0500

    Initial commit

---
+++ app/src/main/java/uz/mobiler/git/MainActivity.kt
@@ -0,0 +1,16 @@
+package uz.mobiler.git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // github
+        // gitlab
+        // bitbucket
+
+    }
+}
