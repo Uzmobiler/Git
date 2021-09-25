c20d8b3370040444c06c8b4e7b61f1e85558d045
sanjar_suvonov <sanjar.tatu.96@gmail.com>
2021-09-25 12:52:13 +0500

    add library

--- app/src/main/java/uz/mobiler/git/MainActivity.kt
+++ app/src/main/java/uz/mobiler/git/MainActivity.kt
@@ -2,11 +2,12 @@ package uz.mobiler.git
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.mobiler.pdptoastlib.ToastMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        
     }
 }
